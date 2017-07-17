3517815acdbef1bbff682cf3a36b0c4e77559e20
andrenaMatthiasZ <matthias.zeleny@andrena.de>
2017-07-17 11:43:25 +0200

    Create CaseIsIgnored test method

---
+++ Anagrams/AnagramsTests/Util/StringExtensionTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Anagrams.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagrams.Util.Tests
+{
+    [TestClass()]
+    public class StringExtensionTestCalculateAnagramKey
+    {
+        [TestMethod()]
+        public void CaseIsIgnored()
+        {
+            Assert.Equals("a".CalculateAnagramKey(), "A".CalculateAnagramKey());
+        }
+    }
+}
