1627768aa38b5e09aab90ecead6fb1e2e7454364
andrenaMatthiasZ <matthias.zeleny@andrena.de>
2017-07-19 10:56:28 +0200

    Write Main method

--- Anagrams/Anagrams/Program.cs
+++ Anagrams/Anagrams/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,29 @@ namespace Anagrams
     {
         static void Main(string[] args)
         {
+            AnagramContainer container = new AnagramContainer();
+
+            try
+            {  
+                using (StreamReader streamReader = new StreamReader("Words.txt", Encoding.GetEncoding("iso-8859-1")))
+                {
+                    string line;
+                    while((line= streamReader.ReadLine())!=null)
+                    {
+                        container.Add(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            string message = container.ToString();
+
+            Console.WriteLine(message);
+            Console.ReadKey();
         }
     }
 }
