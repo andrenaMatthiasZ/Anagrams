5fc2cff0494d1df723df002ff5637f52e5ee4c3f
andrenaMatthiasZ <matthias.zeleny@andrena.de>
2017-08-08 10:17:27 +0200

    Remove  unnecessary imports

--- Anagrams/AnagramsTests/Util/StringExtensionTests.cs
+++ Anagrams/AnagramsTests/Util/StringExtensionTests.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Anagrams.Util;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Anagrams.Util.Tests
 {
